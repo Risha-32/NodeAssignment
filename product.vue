0b35325cfc2c10070e6ec0f5cc9588dc3704ef5a
Risha-32 <119807963+Risha-32@users.noreply.github.com>
2023-01-04 22:21:32 +0530

    Add files via upload

---
+++ product.vue
@@ -0,0 +1,39 @@
+<script>
+
+// export default {
+//  props:['']
+// }
+</script>
+
+<template>
+
+<h2>Product Catalogue</h2> 
+
+<table style="width:100%">
+  <tr>
+    <th>Product Name</th>
+    <th>Product Rate in Rs.</th>
+  </tr>
+  <tr>
+    <td>Pen</td>
+    <td>10</td>
+  </tr>
+  <tr>
+    <td>Pencil</td>
+    <td>5</td>
+  </tr>
+  <tr>
+    <td>Paint</td>
+    <td>50</td>
+  </tr>
+  
+</table>
+
+
+</template>
+
+<style>
+table, th, td {
+  border:1px solid red;
+}
+</style>
